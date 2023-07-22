ed76d1f78c7298750d131f69e8ef8461382deb9a
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2023-07-22 10:07:20 -0700

    new update

--- client/src/components/carD.vue
+++ client/src/components/carD.vue
@@ -1,6 +1,6 @@
 <template>
      <div class="w-64  relative">
-    <img src="src/assets/images/bg.jpg" class="w-full h-48" alt="">
+    <img src="/src/assets/images/bg.jpg" class="w-full h-48" alt="">
     <div class="w-full bgwork2 flex justify-center  items-center h-48 absolute top-0 left-0 " >  
       <div class="absolute  iconspage top-1/2 left-1/3">
       <a href="#jbdv">
