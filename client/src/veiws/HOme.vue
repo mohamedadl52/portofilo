08a0d9c942ad813e67df5f099ebcad741369131b
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2023-07-22 12:29:41 -0700

    new update

--- client/src/veiws/HOme.vue
+++ client/src/veiws/HOme.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="overflow-hidden">
-    <div :style="{ backgroundImage: 'url(' + './src/assets//images//key7.jpg' + ')' }" class="background overflow-hidden">
+    <div :style="{ backgroundImage: 'url(' + './src/assets//images//2.png' + ')' }" class="background overflow-hidden">
        <img class="w-full lg:hidden" height="20px" src="../assets//images//key7.jpg" alt="">
        
        
